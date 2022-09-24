30d1680b53fc53336b074a9fb55be56aa2f8cdf4
Fabricio-Guima <fsgkof@gmail.com>
2022-09-24 10:48:02 -0300

    extension

---
+++ dart_poo/lib/19_extension/1_extension.dart
@@ -0,0 +1,10 @@
+// ignore: file_names
+import 'package:dart_poo/19_extension/saudacao_string_extension.dart';
+
+void main() {
+//extensions = capacidade de vc adicionar funcoes dentro de classes que vc nao tem o código fonte kkkkk
+
+  var nome = 'fafa';
+
+  print(nome.saudacao());
+}
---
+++ dart_poo/lib/19_extension/saudacao_string_extension.dart
@@ -0,0 +1,8 @@
+extension SaudacaoStringExtension on String {
+//adicionando funcoes em String que é do dart kkk
+
+  String saudacao() {
+    //esse this é a String
+    return 'Ola $this, bem vindo';
+  }
+}
