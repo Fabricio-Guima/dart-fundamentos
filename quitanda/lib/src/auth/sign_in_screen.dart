ad8ebdfea0fab4ad8e69adda28c64ea3909f6ade
Fabricio-Guima <fsgkof@gmail.com>
2022-11-11 08:53:37 -0300

    divisor e botao criar conta

--- quitanda/lib/src/auth/sign_in_screen.dart
+++ quitanda/lib/src/auth/sign_in_screen.dart
@@ -14,55 +14,100 @@ class SignInScreen extends StatelessWidget {
             color: Colors.red,
           ),
         ),
-        Expanded(
-          child: Container(
-            padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 40),
-            decoration: const BoxDecoration(
-                color: Colors.white,
-                borderRadius:
-                    const BorderRadius.vertical(top: Radius.circular(40))),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                //email
-                const CustomTextField(
-                  icon: Icons.email,
-                  label: 'Email',
-                ),
-                //senha
-                const CustomTextField(
-                  icon: Icons.lock,
-                  label: 'Senha',
-                  isSecret: true,
-                ),
-                SizedBox(
-                  height: 50,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(18),
-                      ),
+        Container(
+          padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 40),
+          decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius:
+                  const BorderRadius.vertical(top: Radius.circular(40))),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              //email
+              const CustomTextField(
+                icon: Icons.email,
+                label: 'Email',
+              ),
+              //senha
+              const CustomTextField(
+                icon: Icons.lock,
+                label: 'Senha',
+                isSecret: true,
+              ),
+              //Botão entrar
+              SizedBox(
+                height: 50,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(18),
                     ),
+                  ),
+                  onPressed: () {},
+                  child: const Text(
+                    'Entrar',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ),
+              ),
+              Align(
+                alignment: Alignment.centerRight,
+                child: TextButton(
                     onPressed: () {},
                     child: const Text(
-                      'Entrar',
-                      style: TextStyle(fontSize: 18),
+                      'Esqueceu a senha?',
+                      style: TextStyle(color: Colors.red),
+                    )),
+              ),
+              //divisor
+              Padding(
+                padding: const EdgeInsets.only(bottom: 10),
+                child: Row(
+                  children: [
+                    //expanded aqui para a linha aparecer
+                    Expanded(
+                      child: Divider(
+                        color: Colors.grey.withAlpha(90),
+                        thickness: 2,
+                      ),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 15),
+                      child: Text('Ou'),
+                    ),
+                    Expanded(
+                      child: Divider(
+                        color: Colors.grey.withAlpha(90),
+                        thickness: 2,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              //criar conta
+              SizedBox(
+                height: 50,
+                child: OutlinedButton(
+                  style: OutlinedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18)),
+                    side: const BorderSide(
+                      width: 2,
+                      color: Colors.green,
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: const Text(
+                    'Criar conta',
+                    style: TextStyle(
+                      fontSize: 18,
                     ),
                   ),
                 ),
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: TextButton(
-                      onPressed: () {},
-                      child: const Text(
-                        'Esqueceu a senha?',
-                        style: TextStyle(color: Colors.red),
-                      )),
-                )
-              ],
-            ),
+              )
+            ],
           ),
-        )
+        ),
       ]),
     );
   }
