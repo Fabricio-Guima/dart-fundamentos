7c329e3da0475c2b1d58d9a096a310a6ef0d6cd0
Fabricio-Guima <fsgkof@gmail.com>
2022-11-11 08:26:35 -0300

    Botao entrar e esqueceu senha

--- quitanda/lib/src/auth/sign_in_screen.dart
+++ quitanda/lib/src/auth/sign_in_screen.dart
@@ -16,23 +16,49 @@ class SignInScreen extends StatelessWidget {
         ),
         Expanded(
           child: Container(
-            padding: EdgeInsets.symmetric(horizontal: 32, vertical: 40),
-            decoration: BoxDecoration(
+            padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 40),
+            decoration: const BoxDecoration(
                 color: Colors.white,
-                borderRadius: BorderRadius.vertical(top: Radius.circular(40))),
+                borderRadius:
+                    const BorderRadius.vertical(top: Radius.circular(40))),
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
                 //email
-                CustomTextField(
+                const CustomTextField(
                   icon: Icons.email,
                   label: 'Email',
                 ),
                 //senha
-                CustomTextField(
+                const CustomTextField(
                   icon: Icons.lock,
                   label: 'Senha',
                   isSecret: true,
                 ),
+                SizedBox(
+                  height: 50,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18),
+                      ),
+                    ),
+                    onPressed: () {},
+                    child: const Text(
+                      'Entrar',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: TextButton(
+                      onPressed: () {},
+                      child: const Text(
+                        'Esqueceu a senha?',
+                        style: TextStyle(color: Colors.red),
+                      )),
+                )
               ],
             ),
           ),
