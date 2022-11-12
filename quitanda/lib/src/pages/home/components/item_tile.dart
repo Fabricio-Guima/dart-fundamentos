26fae5f8b7c86a8a74962e0a345f7e213bc11382
Fabricio-Guima <fsgkof@gmail.com>
2022-11-12 19:59:32 -0300

    ajustes no card e criação de botao add ao carrinho

--- quitanda/lib/src/pages/home/components/item_tile.dart
+++ quitanda/lib/src/pages/home/components/item_tile.dart
@@ -1,63 +1,94 @@
 import 'package:flutter/material.dart';
 import 'package:quitanda/src/config/custom_colors.dart';
 import 'package:quitanda/src/models/item_model.dart';
+import 'package:quitanda/src/services/utils_services.dart';
 
 class ItemTile extends StatelessWidget {
   final ItemModel item;
 
-  const ItemTile({
+  ItemTile({
     Key? key,
     required this.item,
   }) : super(key: key);
 
+  final UtilsServices utilsServices = UtilsServices();
+
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 1,
-      shadowColor: Colors.grey.shade300,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(20),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(12),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            //imagem
-            //expanded aqui força a imagem crescer até onde der
-            Expanded(child: Image.asset(item.imgUrl)),
-            //nome
-            Text(
-              item.itemName,
-              style: const TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            //preço - unidade
-            Row(
+    return Stack(
+      children: [
+        Card(
+          elevation: 1,
+          shadowColor: Colors.grey.shade300,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(12),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
+                //imagem
+                //expanded aqui força a imagem crescer até onde der
+                Expanded(child: Image.asset(item.imgUrl)),
+                //nome
                 Text(
-                  item.price.toString(),
-                  style: TextStyle(
-                    fontSize: 20,
+                  item.itemName,
+                  style: const TextStyle(
+                    fontSize: 16,
                     fontWeight: FontWeight.bold,
-                    color: CustomColors.customSwatchColor,
                   ),
                 ),
-                Text(
-                  '/${item.unit}',
-                  style: TextStyle(
-                    color: Colors.grey.shade500,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 12,
-                  ),
-                )
+                //preço - unidade
+                Row(
+                  children: [
+                    Text(
+                      //formatando o preço para brl
+                      utilsServices.priceToCurrency(item.price),
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: CustomColors.customSwatchColor,
+                      ),
+                    ),
+                    Text(
+                      '/${item.unit}',
+                      style: TextStyle(
+                        color: Colors.grey.shade500,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 12,
+                      ),
+                    )
+                  ],
+                ),
               ],
             ),
-          ],
+          ),
         ),
-      ),
+        Positioned(
+          top: 4,
+          right: 4,
+          child: GestureDetector(
+            onTap: () {},
+            child: Container(
+              height: 40,
+              width: 35,
+              decoration: BoxDecoration(
+                color: CustomColors.customSwatchColor,
+                borderRadius: const BorderRadius.only(
+                  bottomLeft: Radius.circular(15),
+                  topRight: Radius.circular(15),
+                ),
+              ),
+              child: const Icon(
+                Icons.add_shopping_cart_outlined,
+                color: Colors.white,
+                size: 20,
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
---
+++ quitanda/lib/src/services/utils_services.dart
@@ -0,0 +1,9 @@
+import 'package:intl/intl.dart';
+
+class UtilsServices {
+  String priceToCurrency(double price) {
+    NumberFormat numberFormat = NumberFormat.simpleCurrency(locale: 'pt_BR');
+
+    return numberFormat.format(price);
+  }
+}
