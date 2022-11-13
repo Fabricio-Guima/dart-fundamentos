7ff4ff112ca9559134e21f8105d713b890c13894
Fabricio-Guima <fsgkof@gmail.com>
2022-11-12 22:40:59 -0300

    tela de detalhes do produto

--- quitanda/lib/src/pages/product/product_screen.dart
+++ quitanda/lib/src/pages/product/product_screen.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:quitanda/src/config/custom_colors.dart';
 import 'package:quitanda/src/models/item_model.dart';
+import 'package:quitanda/src/services/utils_services.dart';
 
 class ProductScreen extends StatelessWidget {
-  const ProductScreen({
+  ProductScreen({
     Key? key,
     required this.item,
   }) : super(key: key);
 
   final ItemModel item;
+  final UtilsServices utilsServices = UtilsServices();
 
   @override
   Widget build(BuildContext context) {
@@ -22,6 +25,7 @@ class ProductScreen extends StatelessWidget {
           ),
           Expanded(
             child: Container(
+              padding: const EdgeInsets.all(32),
               decoration: BoxDecoration(
                 color: Colors.white,
                 borderRadius: const BorderRadius.vertical(
@@ -31,6 +35,64 @@ class ProductScreen extends StatelessWidget {
                   BoxShadow(
                     color: Colors.grey.shade600,
                     offset: const Offset(0, 2),
+                  )
+                ],
+              ),
+              child: Column(
+                //esticar esse container na largura máxima
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  // Nome - Quantidade
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Text(
+                          item.itemName,
+                          maxLines: 2,
+                          //colocar 3 pontos ... no final da frase caso ela nao caiba
+                          // na div
+                          overflow: TextOverflow.ellipsis,
+                          style: const TextStyle(
+                            fontSize: 27,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      Container(
+                        height: 30,
+                        width: 70,
+                        color: Colors.red,
+                      )
+                    ],
+                  ),
+                  // Preço
+                  Text(
+                    utilsServices.priceToCurrency(item.price),
+                    style: TextStyle(
+                        fontSize: 23,
+                        fontWeight: FontWeight.bold,
+                        color: CustomColors.customSwatchColor),
+                  ),
+
+                  // Descrição
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10),
+                      child: SingleChildScrollView(
+                        child: Text(
+                          item.description,
+                          style: const TextStyle(
+                            height: 1.5,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+
+                  // botão
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('Botão'),
                   )
                 ],
               ),
