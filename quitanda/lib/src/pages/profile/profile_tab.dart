13a2c090233c85ac3885ed8a244253e14518e0d2
Fabricio-Guima <fsgkof@gmail.com>
2022-11-14 12:10:10 -0300

    modal para confirmar nova senha do usuário

--- quitanda/lib/src/pages/profile/profile_tab.dart
+++ quitanda/lib/src/pages/profile/profile_tab.dart
@@ -2,9 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:quitanda/src/pages/common_widgets/customTextField.dart';
 import 'package:quitanda/src/config/app_data.dart' as appData;
 
-class ProfileTab extends StatelessWidget {
+class ProfileTab extends StatefulWidget {
   const ProfileTab({Key? key}) : super(key: key);
 
+  @override
+  State<ProfileTab> createState() => _ProfileTabState();
+}
+
+class _ProfileTabState extends State<ProfileTab> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -52,14 +57,7 @@ class ProfileTab extends StatelessWidget {
             label: 'CPF',
             readOnly: true,
           ),
-          //atualizar senha
-          CustomTextField(
-            initialValue: appData.user.password,
-            icon: Icons.email,
-            label: 'Senha',
-            isSecret: true,
-            readOnly: false,
-          ),
+          //botao atualizar senha
           SizedBox(
             height: 50,
             child: OutlinedButton(
@@ -71,12 +69,98 @@ class ProfileTab extends StatelessWidget {
                   borderRadius: BorderRadius.circular(20),
                 ),
               ),
-              onPressed: () {},
+              onPressed: () {
+                updatePassword();
+              },
               child: const Text('Atualizar senha'),
             ),
           ),
         ],
       ),
     );
+  }
+
+  //atualizar senha modal
+  Future<bool?> updatePassword() {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return Dialog(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(
+                20,
+              ),
+            ),
+            child: Stack(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: Column(
+                    //esticar os filhos ao máximo que der
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    //reduz o tamanho vertical para o mínimo necessário
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      //título da modal
+                      const Padding(
+                        padding: EdgeInsets.symmetric(
+                          vertical: 12,
+                        ),
+                        child: Text(
+                          'Atualização de senha',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      //input senha
+                      const CustomTextField(
+                        icon: Icons.lock,
+                        label: 'Senha atual',
+                        isSecret: true,
+                      ),
+                      //input nova senha
+                      const CustomTextField(
+                        icon: Icons.lock_outline,
+                        label: 'Nova senha',
+                        isSecret: true,
+                      ),
+                      //input confirmar senha
+                      const CustomTextField(
+                        icon: Icons.lock_outline,
+                        label: 'Confirmar nova senha',
+                        isSecret: true,
+                      ),
+
+                      //botao para mudar senha
+                      SizedBox(
+                        height: 45,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          )),
+                          onPressed: () {},
+                          child: const Text('Atualizar'),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Positioned(
+                    top: 5,
+                    right: 5,
+                    child: IconButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      icon: const Icon(Icons.close),
+                    ))
+              ],
+            ),
+          );
+        });
   }
 }
